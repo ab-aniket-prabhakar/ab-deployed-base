7f57754f875f754432b3fa32653ca92aee652585
Aniket Prabhakar <aniket.p@anderbaher.com>
2025-12-02 07:51:31 +0530

    fixed changes

--- web-greendesk/flutter_bootstrap.js
+++ web-greendesk/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2065143288"
+    serviceWorkerVersion: "2189193603"
   }
 });
